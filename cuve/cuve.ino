3a180e9b6ed2e0d7d0a69b62f9370f5361f6d938
Arnaud Morin <arnaud.morin@gmail.com>
2017-07-11 23:28:14 +0200

    Ajout fonction pour calcul pourcentage cuve
    calcul du pourcentage de la cuve avec une bonne formule magique

--- cuve/cuve.ino
+++ cuve/cuve.ino
@@ -2,7 +2,7 @@
 #include <SoftwareSerial.h>
 
 // Les variables
-int cuve_pleine = 160;    // hauteur en cm quand la cuve est pleine
+float cuve_pleine = 164;  // hauteur en cm quand la cuve est pleine
 int capteur = 170;        // hauteur en cm du capteur
 int trig = 9;             // PIN9 branché sur le TRIP du module (vert)
 int echo = 8;             // PIN8 branché sur l'ECHO du module (bleu)
@@ -10,6 +10,7 @@ int lcd_rx = 2;           // PIN2 branché sur RX LCD (jaune)
 int lcd_tx = 3;           // PIN3 branché sur TX LCD (non utilisée)
 long echo_lu;             // le temps aller/retour de l'impulsion en us
 long cm;                  // la hauteur
+float pourcent;           // la hauteur en pourcentage
 char string[3];           // string array pour le LCD
 
 // Attach the serial display's RX line to digital pin
@@ -26,6 +27,27 @@ void setup()
   delay(1000);            // wait for display to boot up
 }
 
+/*
+ * Fonction calcul volume cuve horizontale
+ * h = hauteur du liquide dans la cuve
+ * d = diametre de la cuve
+ */
+float volCuve(float h, float d){
+  //surfaceSecteur = 1/2*r**2*(2*alpha - sin(2*alpha))
+  float r = 0.0;
+  float alpha = 0.0;
+  float volPourc = 0.0;
+  r = d/2;
+  alpha = acos((r - h) / r);
+  volPourc = ((pow(r,2)/2 * (2*alpha - sin(2*alpha))) / (3.14159*pow(r,2)))*100.0;
+  //ceinture et bretelles
+  if(volPourc <= 0.0)
+    volPourc = 0.0;
+  else if(volPourc > 100.0)
+    volPourc = 100.0;
+  return volPourc;
+}
+
 void loop()
 {
   digitalWrite(trig, HIGH);       // on envoi une impulsion...
@@ -35,6 +57,7 @@ void loop()
   
   // Calculs
   cm = capteur - (echo_lu / 50);
+  pourcent = volCuve(float(cm), cuve_pleine);
   
   // Debug on console
   Serial.print(cm);
@@ -56,7 +79,7 @@ void loop()
   lcd.print("Hauteur cuve :  ");
   lcd.print(cm);
   lcd.print(" cm / ");
-  lcd.print(cm * 100 / cuve_pleine);
+  lcd.print(pourcent);
   lcd.print(" %");
 
   // Attend une seconde avant prochain check
