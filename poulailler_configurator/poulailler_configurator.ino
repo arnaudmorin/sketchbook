6e594b2a4e13cee04834e6a42c1da8fafd3e40fe
Arnaud Morin <arnaud.morin@gmail.com>
2020-04-10 00:33:28 +0200

    Update poulailler configurator
    Signed-off-by: Arnaud Morin <arnaud.morin@gmail.com>

--- poulailler_configurator/poulailler_configurator.ino
+++ poulailler_configurator/poulailler_configurator.ino
@@ -19,9 +19,6 @@ const static uint8_t RADIO_ID = 0;             // Our radio's id.
 const static uint8_t DESTINATION_RADIO_ID = 1; // Id of the radio we will transmit to.
 const static uint8_t PIN_RADIO_CE = 9;
 const static uint8_t PIN_RADIO_CSN = 10;
-const static uint8_t RADIO_CHANNEL = 1;
-String action;
-int test;
 
 /*
  * Paquet sortant, emis par le poulailler
@@ -59,89 +56,38 @@ PaquetSortant paquetSortant;
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  
+  paquetSortant.mode = 1;   // 1 = auto / 2 = fermeture / 3 = ouverture
+  paquetSortant.compteur_nuit_max = 450;
+  paquetSortant.jour = 51;
+  paquetSortant.hyst = 21;
 
-  // By default, 'init' configures the radio to use a 2MBPS bitrate on channel 100 (channels 0-125 are valid).
-  // Both the RX and TX radios must have the same bitrate and channel to communicate with each other.
-  // You can run the 'ChannelScanner' example to help select the best channel for your environment.
-  // You can assign a different bitrate and channel as shown below.
-  //   _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE250KBPS, 0)
-  //   _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE1MBPS, 75)
-  //   _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE2MBPS, 100) // THE DEFAULT
-
-  _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE250KBPS, RADIO_CHANNEL);
+  _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN);
 }
 
 void loop()
 {
-  paquetSortant.mode = 1;
-  paquetSortant.compteur_nuit_max = 449;
-  paquetSortant.jour = 51;
-  paquetSortant.hyst = 21;
+  while (_radio.hasData()){
+    Serial.println("Received:");
+    _radio.readData(&paquetEntrant); // Note how '&' must be placed in front of the variable name.
+    Serial.print(" capteur_photo     = ");
+    Serial.println(paquetEntrant.capteur_photo);
+    Serial.print(" capteur_haut      = ");
+    Serial.println(paquetEntrant.capteur_haut);
+    Serial.print(" capteur_bas       = ");
+    Serial.println(paquetEntrant.capteur_bas);
+    Serial.print(" compteur_nuit     = ");
+    Serial.println(paquetEntrant.compteur_nuit);
+    Serial.print(" compteur_nuit_max = ");
+    Serial.println(paquetEntrant.compteur_nuit_max);
+    Serial.print(" jour              = ");
+    Serial.println(paquetEntrant.jour);
+    Serial.print(" hyst              = ");
+    Serial.println(paquetEntrant.hyst);
+    Serial.print(" mode              = ");
+    Serial.println(paquetEntrant.mode);
 
-  // By default, 'send' transmits data and waits for an acknowledgement.  If no acknowledgement is received,
-  // it will try again up to 16 times.  You can also perform a NO_ACK send that does not request an acknowledgement.
-  // The data packet will only be transmitted a single time so there is no guarantee it will be successful.  Any random
-  // electromagnetic interference can sporatically cause packets to be lost, so NO_ACK sends are only suited for certain
-  // types of situations, such as streaming real-time data where performance is more important than reliability.
-  //   _radio.send(DESTINATION_RADIO_ID, &_radioData, sizeof(_radioData), NRFLite::NO_ACK)
-  //   _radio.send(DESTINATION_RADIO_ID, &_radioData, sizeof(_radioData), NRFLite::REQUIRE_ACK) // THE DEFAULT
-
-  // Read data from serial
-  if (Serial.available() > 0) {
-    action = Serial.readString();
-    Serial.print("action: ");
-    Serial.println(action);
-    test = paquetSortant.jour;
-    Serial.println(test);
+    _radio.addAckData(&paquetSortant, sizeof(paquetSortant));
   }
-
-  //switch(tolower(action[0])) {
-    //case 'r':
-      //Serial.println("Checking if data are received");
-      
-      while (_radio.hasData()){
-        Serial.println("Received:");
-        _radio.readData(&paquetEntrant); // Note how '&' must be placed in front of the variable name.
-        Serial.print(" capteur_photo     = ");
-        Serial.println(paquetEntrant.capteur_photo);
-        Serial.print(" capteur_haut      = ");
-        Serial.println(paquetEntrant.capteur_haut);
-        Serial.print(" capteur_bas       = ");
-        Serial.println(paquetEntrant.capteur_bas);
-        Serial.print(" compteur_nuit     = ");
-        Serial.println(paquetEntrant.compteur_nuit);
-        Serial.print(" compteur_nuit_max = ");
-        Serial.println(paquetEntrant.compteur_nuit_max);
-        Serial.print(" jour              = ");
-        Serial.println(paquetEntrant.jour);
-        Serial.print(" hyst              = ");
-        Serial.println(paquetEntrant.hyst);
-        Serial.print(" mode              = ");
-        Serial.println(paquetEntrant.mode);
-      }
-      
-    //break;
-    //case 'c':
-      //Serial.println("Trying to send config");
-
-      /*
-      if (_radio.send(DESTINATION_RADIO_ID, &paquetSortant, sizeof(paquetSortant))){
-        Serial.println("...Success");
-      }
-      else {
-        Serial.println("...Failed");
-      }
-      */
-    //break;
-    //default:
-    //  Serial.println("Nothing to do");
-    //break;
-  //}
-
-  // Reset action
-  //action = "";
-
-  // Wait a bit before next loop
-  delay(200);
 }
